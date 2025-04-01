158f5626dfc38383d6661cc0633e2c3e7ba0ada1
Benedikt Mehl <b.mehl14@outlook.de>
2025-04-01 23:55:46 +0200

    auto commit

--- src/fetchContent/test_fetch_html_content.py
+++ src/fetchContent/test_fetch_html_content.py
@@ -1,20 +1,36 @@
 import unittest
-from src.fetch.html_content_fetcher import HTMLContentFetcher
+from fetch_html_content import fetch_html_contents
 
 
-class TestHTMLContentFetcher(unittest.TestCase):
-    def test_get_html_content(self):
-        url = "https://victorpoughon.fr/i-tried-making-artificial-sunlight-at-home/"
-        fetcher = HTMLContentFetcher()
-        content = fetcher.get_html_content(url)
-        self.assertTrue(isinstance(content, str))
-        self.assertGreater(len(content), 0)
+class TestFetchHTMLContent(unittest.TestCase):
+    def test_fetch_html_contents(self):
+        topics = [
+            {'title': 'Wikipedia', 'url': 'https://www.wikipedia.org/'},
+            {'title': 'Python', 'url': 'https://www.python.org/'},
+        ]
+        max_number_of_contents = 2
 
-    def test_print_html_content(self):
-        url = "https://victorpoughon.fr/i-tried-making-artificial-sunlight-at-home/"
-        fetcher = HTMLContentFetcher()
-        content = fetcher.get_html_content(url)
-        print(content)
+        contents = fetch_html_contents(topics, max_number_of_contents)
+
+        self.assertEqual(len(contents), 2)
+        self.assertEqual(contents[0]['title'], 'Wikipedia')
+        self.assertEqual(contents[0]['url'], 'https://www.wikipedia.org/')
+        self.assertTrue(isinstance(contents[0]['content'], str))
+        self.assertGreater(len(contents[0]['content']), 0)
+
+        self.assertEqual(contents[1]['title'], 'Python')
+        self.assertEqual(contents[1]['url'], 'https://www.python.org/')
+        self.assertTrue(isinstance(contents[1]['content'], str))
+        self.assertGreater(len(contents[1]['content']), 0)
+
+    def test_fetch_html_contents_short_raw_text(self):
+        topics = [
+            {'title': 'No Content', 'url': 'https://httpbin.org/status/404'},  # URL that returns a 404 error
+        ]
+
+        contents = fetch_html_contents(topics)
+
+        self.assertEqual(len(contents), 0)  # No content should be added due to invalid response
 
 
 if __name__ == '__main__':
