8e0cd8184237d08a0c1b35a4077bc96b9eb7626f
Benedikt Mehl <b.mehl14@outlook.de>
2025-04-18 14:45:53 +0200

    auto commit

--- src/send/test_send_telegram_message.py
+++ src/send/test_send_telegram_message.py
@@ -2,10 +2,12 @@ import unittest
 import asyncio
 from src.send.send_telegram_message import send_post_to_telegram
 
+
 class TestSendTelegramMessage(unittest.TestCase):
     def test_send_post_to_telegram(self):
         output_dir = f"posts/test-10-10-2010"
         asyncio.run(send_post_to_telegram(output_dir))
+
 
 if __name__ == '__main__':
     unittest.main()
